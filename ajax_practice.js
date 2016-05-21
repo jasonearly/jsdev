dc670f903c5df7901b8dd22f2c9999d9224ef0e0
Jason Early <theoffice@gruntmonkey.com>
2016-05-21 11:23:11 -0500

    ajax/handlebars practice

--- ajax_practice.js
+++ ajax_practice.js
@@ -10,28 +10,79 @@ $.ajax({
 
 
 
-$(function (get_data) {
-  var Data = $.ajax({
-		type: 'GET',
-		url: 'http://api.openweathermap.org/data/2.5/weather?zip=60647,us&APPID=aa03e03b0f01411758a535d4ed1b2492',
-		 success: function(Data) {
-        console.log(Data)
-	}});
+// $(function (get_data) {
+//   var Data =
+//   });
 
-  //{name:"Nike", price:199.00 };
 
-   //Get the HTML from the template   in the script tag​
-  var theTemplateScript = $('#temp-template').html();
+ //  $.ajax({
+	// 	type: 'GET',
+	// 	url: 'http://api.openweathermap.org/data/2.5/weather?zip=60647,us&APPID=aa03e03b0f01411758a535d4ed1b2492',
+	// 	 //pass the success/return into a var. (review solution)
+	// 	success: function(response) {
+ //        console.log(response);
+	// 		}
+	// });
 
-   //Compile the template​
-  var theTemplate = Handlebars.compile (theTemplateScript);
-  $('#weather').append(theTemplate(get_data));
+
+//Attempt at Handlebars solution
+Handlebars.registerHelper('toFahrenheight', function(kelvin) {
+	return ((kelvin - 273) * (9/5) + 32).toFixed(1) + '\u00B0F';
+});
+
+function processHandlebarsTemplate(templateId, data) {
+	var templateSource = $('#' + templateId).html();
+	var compiledTemplate = Handlebars.compile(templateSource);
+
+	return compiledTemplate(data);
+}
+
+$.ajax({
+	type: 'GET',
+	url: 'http://api.openweathermap.org/data/2.5/weather?zip=60647,us&APPID=e0348244f8198c705d7e3a4d429a4e93',
+	success: function(response) {
+		var weatherHtml = processHandlebarsTemplate('temp-template', response);
+		$('#weather').html(weatherHtml);
+	}
+});
+
+
+ // Notes on Handlebars
+ // Get the HTML from the template   in the script tag​
+ // var theTemplateScript = $('#temp-template').html();
+
+ //Compile the template​
+ //  var theTemplate = Handlebars.compile (theTemplateScript);
+ //  $('#weather').append(theTemplate(response));
 
 //We pass the object to the compiled handleBars function​
 // The function will insert all the values from the objects in their respective places in the HTML and returned HTML as a string.
 //Then we use jQuery to append the resulting HTML string into the page​
-});
 
 
+// //solution via Gordo
+// Handlebars.registerHelper('toFahrenheight', function(kelvin) {
+// 	return ((kelvin - 273) * (9/5) + 32).toFixed(1) + '\u00B0F'
+// })
 
+// // This uses a library called moment.js
+// Handlebars.registerHelper('formatDate', function(dateText) {
+// 	return moment(dateText).format('dddd MMM Do [at] ha');
+// })
+
+// function processHandlebarsTemplate(templateId, data) {
+// 	var templateSource = $('#' + templateId).html();
+// 	var compiledTemplate = Handlebars.compile(templateSource);
+
+// 	return compiledTemplate(data);
+// }
+
+// $.ajax({
+// 	type: 'GET',
+// 	url: 'http://api.openweathermap.org/data/2.5/forecast?zip=60657,us&APPID=e0348244f8198c705d7e3a4d429a4e93',
+// 	success: function(response) {
+// 		var weatherHtml = processHandlebarsTemplate('forecast', response);
+// 		$('#weather').html(weatherHtml);
+// 	}
+// })
 
