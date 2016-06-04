66947fd18e30301bca1dfe638638374f3b1cb023
Jason Early <theoffice@gruntmonkey.com>
2016-06-04 17:17:12 -0500

    popup article info

--- projects/feedr/js/app.js
+++ projects/feedr/js/app.js
@@ -6,9 +6,19 @@
 var redditLogo = "https://camo.githubusercontent.com/b13830f5a9baecd3d83ef5cae4d5107d25cdbfbe/68747470733a2f2f662e636c6f75642e6769746875622e636f6d2f6173736574732f3732313033382f313732383830352f35336532613364382d363262352d313165332d383964312d3934376632373062646430332e706e67";
 var googleNews = "https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/1024px-Google_%22G%22_Logo.svg.png";
 
+//initial load
 $(window).on('load', function() {
     $('#popUp a').hide();
     $('#popUp').removeClass('hidden');
+});
+
+//esc key closes popUp
+$(document).keyup(function(e) {
+    //needs to be on page load
+    if (e.which === 27) {
+        $('#popUp').addClass('hidden');
+        $('#popUp a').show();
+    }
 });
 
 //Loads data feed
@@ -21,43 +31,61 @@ $(window).on('load', function() {
 //     })
 // })
 
+
+
 $(function initialDigg() {
-$.ajax({
-    type: 'GET',
-    url: 'https://accesscontrolalloworiginall.herokuapp.com/http://digg.com/api/news/popular.json',
-    success: function(results) {
-        results.data.feed.forEach(function(result) {
+    $.ajax({
+        type: 'GET',
+        url: 'https://accesscontrolalloworiginall.herokuapp.com/http://digg.com/api/news/popular.json',
+        success: function(results) {
+            results.data.feed.forEach(function(result) {
 
-            //HANDLBARS THIS
-            $("#main").append('<article class="article"><section class="featuredImage"><img src="' + result.content.media.images[0].url + '" alt="" /></section><section class="articleContent"><a href="' + result.content.url + '"><h3>' + result.content.title + '</h3></a><h6>' + result.content.tags[0].display + '</h6></section><section class="impressions">' + result.diggs.count + '</section><div class="clearfix"></div></article>')
-        })
-        $('#popUp').addClass('hidden');
-    }
-});
+                //HANDLBARS THIS
+                $("#main").append('<article class="article"><section class="featuredImage"><img src="' + result.content.media.images[0].url + '" alt="" /></section><section class="articleContent"><a href="' + result.content.url + '"><h3>' + result.content.title + '</h3></a><h6>' + result.content.tags[0].display + '</h6></section><section class="impressions">' + result.diggs.count + '</section><div class="clearfix"></div></article>')
+
+
+                $('.articleContent a').on('click', function(event) {
+                    event.preventDefault();
+                    $('#popUp a').show();
+                    $('#popUp').removeClass('loader');
+                    $('#popUp').removeClass('hidden');
+
+                    $('#popUp .container h1').text(result.content.title);
+                    $('#popUp p').text(result.content.description);
+                    $('#source').attr('href', result.content.url);
+
+                })
+            })
+
+
+            $('#popUp').addClass('hidden');
+        }
+    });
 })
 
-$('#digg').on('click' ,function loadDigg() {
+$('#digg').on('click', function loadDigg() {
     $('#popUp').removeClass('hidden');
-$.ajax({
-    type: 'GET',
-    url: 'https://accesscontrolalloworiginall.herokuapp.com/http://digg.com/api/news/popular.json',
-    success: function(results) {
-$("#main").empty();
-        results.data.feed.forEach(function(result) {
+    $.ajax({
+        type: 'GET',
+        url: 'https://accesscontrolalloworiginall.herokuapp.com/http://digg.com/api/news/popular.json',
+        success: function(results) {
+            $("#main").empty();
+            $('#popUp').addClass('hidden');
+            results.data.feed.forEach(function(result) {
 
-            //HANDLBARS THIS
-            $("#main").append('<article class="article"><section class="featuredImage"><img src="' + result.content.media.images[0].url + '" alt="" /></section><section class="articleContent"><a href="' + result.content.url + '"><h3>' + result.content.title + '</h3></a><h6>' + result.content.tags[0].display + '</h6></section><section class="impressions">' + result.diggs.count + '</section><div class="clearfix"></div></article>')
-        })
-        $('#popUp').addClass('hidden');
-    }
+                //HANDLBARS THIS
+                $("#main").append('<article class="article"><section class="featuredImage"><img src="' + result.content.media.images[0].url + '" alt="" /></section><section class="articleContent"><a href="' + result.content.url + '"><h3>' + result.content.title + '</h3></a><h6>' + result.content.tags[0].display + '</h6></section><section class="impressions">' + result.diggs.count + '</section><div class="clearfix"></div></article>')
+            });
+            //$('#popUp').addClass('hidden');
+        }
+    });
 });
-})
 
 
-$('#reddit').on('click' , function loadReddit() {
+$('#reddit').on('click', function loadReddit() {
     //    var url = 'http://www.theverge.com/apple/rss/index.xml';
     //var url = 'https://www.reddit.com/r/news.json';
-       $('#popUp').removeClass('hidden');
+    $('#popUp').removeClass('hidden');
     $.ajax({
         type: "GET",
         //+ encodeURIComponent(url)
@@ -86,7 +114,7 @@ $('#reddit').on('click' , function loadReddit() {
 $('#googleNews').on('click', function loadGoogleNews() {
     //    var url = 'http://www.theverge.com/apple/rss/index.xml';
     //var url = 'https://www.reddit.com/r/news.json';
-       $('#popUp').removeClass('hidden');
+    $('#popUp').removeClass('hidden');
     $.ajax({
         type: "GET",
         //+ encodeURIComponent(url)
@@ -97,7 +125,7 @@ $('#googleNews').on('click', function loadGoogleNews() {
         },
         success: function(results) {
             $("#main").empty();
-$('#popUp').addClass('hidden');
+            $('#popUp').addClass('hidden');
             //alert('It loads!');
             //values = xml.responseData.feed.entries;
             //console.log(values);
@@ -105,6 +133,8 @@ $('#popUp').addClass('hidden');
                 $("#main").append('<article class="article"><section class="featuredImage"><img src="' + googleNews + '"/></section><section class="articleContent"><a href="' + result.link + '"><h3>' + result.title + '</h3></a><h6>' + result.categories + '</h6></section><section class="impressions">count</section><div class="clearfix"></div></article>');
 
             });
+
+
         }
     });
 });
@@ -126,15 +156,37 @@ $('#popUp').addClass('hidden');
 
 
 
+
+
+
+
+
+
 //3. When the user selects an article's title show the #popUp overlay. The content of the article must be inserted in the .container class inside #popUp.
 // on click of title
 // show #popUp
 // insert data into popUp container
+//DONE
+
+
+
+
 
 //4. Make sure you remove the .loader class when toggling the article information in the pop-up.
-
+//Done
 
 //5. Change the link of the "Read more from source" button to that of the respective article.
+//DONE
+
+
+
+
+
+
+
+
+
+
 
 //6. When the user selects a source from the dropdown menu on the header,
 //replace the content of the page with articles from the newly selected source.
@@ -143,7 +195,6 @@ $('#popUp').addClass('hidden');
 //     hide current data source
 //     load/display new data source
 // });
-
 
 //7. Display the loading pop up when the user first selects the new source, and hide it on success.
 //DONE
@@ -174,13 +225,7 @@ $(document).keypress(function(e) {
 //DONE
 
 // //trigger with keypress for test
-// $(document).keypress(function(e) {
-//     //needs to be on page load
-//     if (e.which === 49) {
-//         $('#popUp').toggleClass('hidden');
-//         $('#popUp a').show();
-//     }
-// });
+
 
 
 //12. Add functionality to hide the pop-up when user selects the "X" button on the pop-up.
