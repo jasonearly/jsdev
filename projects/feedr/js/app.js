77aaacb96bd28b2ab6150f9b1102fd66bd82bdce
Jason Early <theoffice@gruntmonkey.com>
2016-06-04 10:41:17 -0500

    reddit feed loads

--- projects/feedr/js/app.js
+++ projects/feedr/js/app.js
@@ -1,11 +1,11 @@
 'use strict';
 /*
   Please add all Javascript code to this file.
-*/
-$(window).on('load', function(){
-    $('#popUp a').removeClass('closePopUp');
+  */
+$(window).on('load', function() {
+    $('#popUp a').hide();
     $('#popUp').removeClass('hidden');
-  });
+});
 
 //Loads data feed
 
@@ -22,11 +22,44 @@ $.ajax({
     url: 'https://accesscontrolalloworiginall.herokuapp.com/http://digg.com/api/news/popular.json',
     success: function(results) {
         results.data.feed.forEach(function(result) {
+
+            //HANDLBARS THIS
             $("#main").append('<article class="article"><section class="featuredImage"><img src="' + result.content.media.images[0].url + '" alt="" /></section><section class="articleContent"><a href="' + result.content.url + '"><h3>' + result.content.title + '</h3></a><h6>' + result.content.tags[0].display + '</h6></section><section class="impressions">' + result.diggs.count + '</section><div class="clearfix"></div></article>')
         })
         $('#popUp').addClass('hidden');
     }
 });
+
+$(function loadReddit() {
+    //    var url = 'http://www.theverge.com/apple/rss/index.xml';
+    //var url = 'https://www.reddit.com/r/news.json';
+    $.ajax({
+        type: "GET",
+        //+ encodeURIComponent(url)
+        url: 'https://accesscontrolalloworiginall.herokuapp.com/https://www.reddit.com/r/news.json',
+        //dataType: 'json',
+        error: function() {
+            alert('Unable to load feed, Incorrect path or invalid feed');
+        },
+        success: function(results) {
+            //alert('It loads!');
+            //values = xml.responseData.feed.entries;
+            //console.log(values);
+            results.data.children.forEach(function(result) {
+                $("#main").append('loading reddit' + ' ' + result.data.title);
+
+
+        });
+
+
+
+
+
+
+        }
+    });
+});
+
 
 //Feed rules
 //1. When the application first loads display the loading container (see below on instructions to toggle this).
@@ -37,21 +70,24 @@ $.ajax({
 //2. When you successfully retrieve information from the default API hide the loader and replace the content of the #main container with that of the API. The DOM structure of each article must adhere to the .article structure.
 //DONE
 
-   //     $(document).ready(function() {
-   //    $('#popUp').addClass('hidden');
-   //    console.log('Popup loaded then was hidden');
-   // });
+//     $(document).ready(function() {
+//    $('#popUp').addClass('hidden');
+//    console.log('Popup loaded then was hidden');
+// });
 
 
 
-//When the user selects an article's title show the #popUp overlay. The content of the article must be inserted in the .container class inside #popUp.
+//3. When the user selects an article's title show the #popUp overlay. The content of the article must be inserted in the .container class inside #popUp.
+// on click of title
+// show #popUp
+// insert data into popUp container
 
-//Make sure you remove the .loader class when toggling the article information in the pop-up.
+//4. Make sure you remove the .loader class when toggling the article information in the pop-up.
 
 
-//Change the link of the "Read more from source" button to that of the respective article.
+//5. Change the link of the "Read more from source" button to that of the respective article.
 
-//When the user selects a source from the dropdown menu on the header,
+//6. When the user selects a source from the dropdown menu on the header,
 //replace the content of the page with articles from the newly selected source.
 
 // $().on('click' function(){
@@ -60,23 +96,23 @@ $.ajax({
 // });
 
 
-//Display the loading pop up when the user first selects the new source, and hide it on success.
+//7. Display the loading pop up when the user first selects the new source, and hide it on success.
 
 
-//Add an error message (either alert or a notification on the page) if the app cannot load from the selected feed.
+//8. Add an error message (either alert or a notification on the page) if the app cannot load from the selected feed.
 
 
 
 //UI interactions
-//When the user clicks/taps the search icon, expand the input box. Best approach for this is to toggle the .active class for the #search container. If the search input box is already expanded tapping the search icon again will close the input.
-
+//9. When the user clicks/taps the search icon, expand the input box. Best approach for this is to toggle the .active class for the #search container. If the search input box is already expanded tapping the search icon again will close the input.
+//DONE
 $('#search_button').click(function() {
     $('#search').toggleClass('active');
 });
 
-//Pressing the "Enter" key should also close the opened input box. See Bonus 2 for search filtering functionality.
+//10. Pressing the "Enter" key should also close the opened input box. See Bonus 2 for search filtering functionality.
 //press enter on text area..
-
+//DONE
 $(document).keypress(function(e) {
     if (e.which === 13) {
         $('#search').removeClass('active');
@@ -85,24 +121,24 @@ $(document).keypress(function(e) {
 
 
 
-//When the app is first loading and when the user selects to load a new feed from the dropdown, display the #popUp container with the .loader class. You can toggle the .hidden class from the container to display/hide the overlay container.
+//11. When the app is first loading and when the user selects to load a new feed from the dropdown, display the #popUp container with the .loader class. You can toggle the .hidden class from the container to display/hide the overlay container.
 
 //trigger with keypress for test
 $(document).keypress(function(e) {
     //needs to be on page load
     if (e.which === 49) {
         $('#popUp').toggleClass('hidden');
+        $('#popUp a').show();
     }
 });
 
 
-//Add functionality to hide the pop-up when user selects the "X" button on the pop-up.
+//12. Add functionality to hide the pop-up when user selects the "X" button on the pop-up.
+//DONE
 $('.closePopUp').click(function() {
     $('#popUp').addClass('hidden');
 });
 
 
-//Clicking/tapping the "Feedr" logo will display the main/default feed.
-
-
-
+//13. Clicking/tapping the "Feedr" logo will display the main/default feed.
+// on click load data source[0]
