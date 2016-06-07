4b551ca2d8b2cb24cb09a153946d912ca1c8e2e9
Jason Early <theoffice@gruntmonkey.com>
2016-06-07 17:02:35 -0500

    objectives complete

--- projects/feedr/js/app.js
+++ projects/feedr/js/app.js
@@ -6,12 +6,12 @@
 var redditLogo = 'https://camo.githubusercontent.com/b13830f5a9baecd3d83ef5cae4d5107d25cdbfbe/68747470733a2f2f662e636c6f75642e6769746875622e636f6d2f6173736574732f3732313033382f313732383830352f35336532613364382d363262352d313165332d383964312d3934376632373062646430332e706e67';
 var googleNews = 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/1024px-Google_%22G%22_Logo.svg.png';
 
-var allowOriginal = 'https://accesscontrolalloworiginall.herokuapp.com/';
 
 //initial load
 $(window).on('load', function() {
-    $('#popUp a').hide();
     $('#popUp').removeClass('hidden');
+    $('#popUp a').hide();
+
 });
 
 //esc key closes popUp
@@ -21,22 +21,12 @@ $(document).keyup(function(e) {
         $('#popUp').addClass('hidden loader');
         $('#popUp a').hide();
         $('#popUp p').show();
-        // $('#popUp .container').show();
-         $('iframe').hide();
+        $('iframe').hide();
     }
 });
 
 
-//Search bar
-$('#search_button').click(function() {
-    $('#search').toggleClass('active');
-});
 
-$(document).keypress(function(e) {
-    if (e.which === 13) {
-        $('#search').removeClass('active');
-    }
-});
 
 //Display this on page load
 $(function initialDigg() {
@@ -51,47 +41,35 @@ $(function initialDigg() {
             results.data.feed.forEach(function(result) {
 
                 //HANDLBARS THIS
-                // $("#main").append('<article class="article"><section class="featuredImage"><img src="' + result.content.media.images[0].url + '" alt="" /></section><section class="articleContent"><a href="' + result.content.url + '"><h3>' + result.content.title + '</h3></a><h6>' + result.content.tags[0].display + '</h6></section><section class="impressions">' + result.diggs.count + '</section><div class="clearfix"></div></article>');
-
                 var $articleTemplate = $('<article class="article"><section class="featuredImage"><img src="' + result.content.media.images[0].url + '" alt="" /></section><section class="articleContent"><a href="' + result.content.url + '"><h3>' + result.content.title + '</h3></a><h6>' + result.content.tags[0].display + '</h6></section><section class="impressions">' + result.diggs.count + '</section><div class="clearfix"></div></article>').appendTo('#main');
 
-                    $articleTemplate.on('click', function(event) {
-                        $('iframe').hide().attr('src', result.content.url);
+                $articleTemplate.on('click', function(event) {
+
+                    event.preventDefault();
+                    $('#popUp .container').show();
+                    $('iframe').hide().attr('src', result.content.url);
+                    $('#popUp a').show();
+                    $('#popUp').removeClass('loader hidden');
+
+                    $('#popUp .container h1').text(result.content.title);
+                    $('#popUp p').text(result.content.description);
+                    $('#source').on('click', function() {
                         event.preventDefault();
-                        $('#popUp a').show();
-                        $('#popUp').removeClass('loader hidden');
-
-                        $('#popUp .container h1').text(result.content.title);
-                        $('#popUp p').text(result.content.description);
-                        //$('#source').attr('href', result.content.url);
-                        $('#source').on('click', function() {
-                            event.preventDefault();
-                            $('#popUp .container').hide();
-                            $('iframe').show();
-                        });
-
-
-
-                        //display source page in #popUp
-                        // $.ajax({
-                        //     type: 'GET',
-                        //     url: result.content.url,
-                        //     // error: function() {
-                        //     //     alert('Unable to load feed, Incorrect path or invalid feed');
-                        //     // },
-                        //     success: function(results) {
-                        //         console.log("source page loads!");
-                        //     }
+                        $('#popUp .container').hide();
+                       $('iframe').show();
                     });
-
                 });
 
-                $('.closePopUp').click(function() {
+                $('.closePopUp').on('click',function() {
+                    //$('#popUp .container').show();
                     $('#popUp').addClass('hidden loader');
                     $('#popUp a').hide();
-                   // $('#popUp .container').show();
                     $('iframe').hide();
                 });
+
+            });
+
+
 
 
             $('#popUp').addClass('hidden');
@@ -101,7 +79,7 @@ $(function initialDigg() {
 
 //load digg feed
 $('.digg').on('click', function loadDigg() {
-   $('#popUp').removeClass('hidden');
+    $('#popUp').removeClass('hidden');
     $('#newsSource').text('Digg');
     $.ajax({
         type: 'GET',
@@ -115,57 +93,33 @@ $('.digg').on('click', function loadDigg() {
             results.data.feed.forEach(function(result) {
 
                 //HANDLBARS THIS
-                // $("#main").append('<article class="article"><section class="featuredImage"><img src="' + result.content.media.images[0].url + '" alt="" /></section><section class="articleContent"><a href="' + result.content.url + '"><h3>' + result.content.title + '</h3></a><h6>' + result.content.tags[0].display + '</h6></section><section class="impressions">' + result.diggs.count + '</section><div class="clearfix"></div></article>');
 
-                 var $diggTemplate = $('<article class="article"><section class="featuredImage"><img src="' + result.content.media.images[0].url + '" alt="" /></section><section class="articleContent"><a href="' + result.content.url + '"><h3>' + result.content.title + '</h3></a><h6>' + result.content.tags[0].display + '</h6></section><section class="impressions">' + result.diggs.count + '</section><div class="clearfix"></div></article>').appendTo("#main");
+                var $diggTemplate = $('<article class="article"><section class="featuredImage"><img src="' + result.content.media.images[0].url + '" alt="" /></section><section class="articleContent"><a href="' + result.content.url + '"><h3>' + result.content.title + '</h3></a><h6>' + result.content.tags[0].display + '</h6></section><section class="impressions">' + result.diggs.count + '</section><div class="clearfix"></div></article>').appendTo("#main");
 
-                 $diggTemplate.on('click', function(event) {
-                    $('iframe').hide().attr('src', result.content.url);
+                $diggTemplate.on('click', function(event) {
+                   $('iframe').hide().attr('src', result.content.url);
+                   $('#popUp .container').show();
+                    event.preventDefault();
+                    $('#popUp a').show();
+                    $('#popUp').removeClass('loader hidden');
+
+                    $('#popUp .container h1').text(result.content.title);
+                    $('#popUp p').text(result.content.description);
+
+                    $('#source').on('click', function() {
                         event.preventDefault();
-                        $('#popUp a').show();
-                        $('#popUp').removeClass('loader hidden');
-
-                        //display source page in #popUp
-                        // $.ajax({
-                        //     type: 'GET',
-                        //     url: result.content.url,
-                        //     // error: function() {
-                        //     //     alert('Unable to load feed, Incorrect path or invalid feed');
-                        //     // },
-                        //     success: function(results) {
-                        //         console.log("source page loads!");
-                        //     }
-
-                        $('#popUp .container h1').text(result.content.title);
-                        $('#popUp p').text(result.content.description);
-                        //$('#source').attr('href', result.content.url);
-
-                         $('#source').on('click', function() {
-                            event.preventDefault();
-                            $('#popUp .container').hide();
-                            $('iframe').show();
-                        });
+                        $('#popUp .container').hide();
+                       $('iframe').show();
                     });
-
-
-                // $('.articleContent a').on('click', function(event) {
-                //     event.preventDefault();
-                //     $('#popUp a').show();
-                //     $('#popUp').removeClass('loader hidden');
-
-                //     $('#popUp .container h1').text(result.content.title);
-                //     $('#popUp p').text(result.content.description);
-                //     $('#source').attr('href', result.content.url);
-
-                // });
+                });
 
                 $('.closePopUp').click(function() {
                     $('#popUp').addClass('hidden loader');
                     $('#popUp a').hide();
-                  //  $('#popUp .container').show();
-                    $('iframe').hide();
+                   $('iframe').hide();
                 });
             });
+
         }
     });
 });
@@ -186,55 +140,31 @@ $('#reddit').on('click', function loadReddit() {
             $('#popUp').addClass('hidden');
 
             results.data.children.forEach(function(result) {
-                // $("#main").append('<article class="article"><section class="featuredImage"><img src="' + redditLogo + '" /></section><section class="articleContent"><a href="' + result.data.url + '"><h3>' + result.data.title + '</h3></a><h6>tags</h6></section><section class="impressions"></section><div class="clearfix"></div></article>');
-
                 var $redditTemplate = $('<article class="article"><section class="featuredImage"><img src="' + redditLogo + '" /></section><section class="articleContent"><a href="' + result.data.url + '"><h3>' + result.data.title + '</h3></a><h6>tags</h6></section><section class="impressions"></section><div class="clearfix"></div></article>').appendTo("#main");
 
                 $redditTemplate.on('click', function(event) {
-                    $('iframe').hide().attr('src', result.data.url);
+                   $('iframe').hide().attr('src', result.data.url);
+                   $('#popUp .container').show();
+                    event.preventDefault();
+                    $('#popUp a').show();
+                    $('#popUp').removeClass('loader hidden');
+
+                    $('#popUp .container h1').text(result.data.title);
+                    $('#popUp p').hide();
+                    $('#source').on('click', function() {
                         event.preventDefault();
-                        $('#popUp a').show();
-                        $('#popUp').removeClass('loader hidden');
-
-                        //display source page in #popUp
-                        // $.ajax({
-                        //     type: 'GET',
-                        //     url: result.content.url,
-                        //     // error: function() {
-                        //     //     alert('Unable to load feed, Incorrect path or invalid feed');
-                        //     // },
-                        //     success: function(results) {
-                        //         console.log("source page loads!");
-                        //     }
-
-
-                        $('#popUp .container h1').text(result.data.title);
-                        $('#popUp p').hide();
-                        //$('#source').attr('href', result.data.url);
-                         $('#source').on('click', function() {
-                            event.preventDefault();
-                            $('#popUp .container').hide();
-                            $('iframe').show();
-                        });
+                        $('#popUp .container').hide();
+                       $('iframe').show();
                     });
-
-                // $('.articleContent a').on('click', function(event) {
-                //     event.preventDefault();
-                //     $('#popUp a').show();
-                //     $('#popUp').removeClass('loader hidden');
-
-                //     $('#popUp .container h1').text(result.data.title);
-                //     $('#popUp p').hide();
-                //     $('#source').attr('href', result.data.url);
-
-                // });
-
+                });
 
                 $('.closePopUp').click(function() {
+
                     $('#popUp').addClass('hidden loader');
+                    // $('#popUp .container').hide();
                     $('#popUp a').hide();
                     $('#popUp p').show();
-                    //$('#popUp .container').show();
+
                     $('iframe').hide();
                 });
             });
@@ -257,55 +187,31 @@ $('#googleNews').on('click', function loadGoogleNews() {
             $('#popUp').addClass('hidden');
 
             results.responseData.feed.entries.forEach(function(result) {
-                // $("#main").append('<article class="article"><section class="featuredImage"><img src="' + googleNews + '"/></section><section class="articleContent"><a href="' + result.link + '"><h3>' + result.title + '</h3></a><h6>' + result.categories + '</h6></section><section class="impressions"></section><div class="clearfix"></div></article>');
-
                 var $googleNewsTemplate = $('<article class="article"><section class="featuredImage"><img src="' + googleNews + '"/></section><section class="articleContent"><a href="' + result.link + '"><h3>' + result.title + '</h3></a><h6>' + result.categories + '</h6></section><section class="impressions"></section><div class="clearfix"></div></article>').appendTo("#main");
 
                 $googleNewsTemplate.on('click', function(event) {
+                    $('#popUp .container').show();
                     $('iframe').hide().attr('src', result.link);
+                    event.preventDefault();
+                    $('#popUp a').show();
+                    $('#popUp').removeClass('loader hidden');
+
+                    $('#popUp .container h1').text(result.title);
+                    $('#popUp p').text(result.contentSnippet);
+                    //$('#source').attr('href', result.link);
+
+                    $('#source').on('click', function() {
                         event.preventDefault();
-                        $('#popUp a').show();
-                        $('#popUp').removeClass('loader hidden');
+                        $('#popUp .container').hide();
+                       $('iframe').show();
 
-                        //display source page in #popUp
-                        // $.ajax({
-                        //     type: 'GET',
-                        //     url: result.content.url,
-                        //     // error: function() {
-                        //     //     alert('Unable to load feed, Incorrect path or invalid feed');
-                        //     // },
-                        //     success: function(results) {
-                        //         console.log("source page loads!");
-                        //     }
-
-
-                        $('#popUp .container h1').text(result.title);
-                        $('#popUp p').text(result.contentSnippet);
-                        //$('#source').attr('href', result.link);
-
-                         $('#source').on('click', function() {
-                            event.preventDefault();
-                            $('#popUp .container').hide();
-                            $('iframe').show();
-                        });
                     });
-
-                // $('.articleContent a').on('click', function(event) {
-                //     event.preventDefault();
-                //     $('#popUp a').show();
-                //     $('#popUp').removeClass('loader hidden');
-
-                //     $('#popUp .container h1').text(result.title);
-                //     $('#popUp p').text(result.contentSnippet);
-                //     $('#source').attr('href', result.link);
-
-                // });
+                });
 
                 $('.closePopUp').click(function() {
                     $('#popUp').addClass('hidden loader');
                     $('#popUp a').hide();
-                    //$('#popUp .container').hide();
-                    $('iframe').hide();
+                   $('iframe').hide();
                 });
 
             });
@@ -317,7 +223,13 @@ $('#googleNews').on('click', function loadGoogleNews() {
 
 
 
+//Search bar
+$('#search_button').click(function() {
+    $('#search').toggleClass('active');
+});
 
-//The app must be able to display the full contents of the article. This can be as simple as an iframe or as fancy as you need it to be.
-
-
+$(document).keypress(function(e) {
+    if (e.which === 13) {
+        $('#search').removeClass('active');
+    }
+});
